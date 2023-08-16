25a71d61fcba590b03fbffbc072423da0b394147
Hussain_Sharif <122522406+Hussain-Sharif@users.noreply.github.com>
2023-08-16 23:14:39 +0530

    Update index.js
    Added tab-selection functionality

--- src/components/TabItem/index.js
+++ src/components/TabItem/index.js
@@ -3,11 +3,22 @@ import {Component} from 'react'
 import './index.css'
 
 class TabItem extends Component {
+  onClick = () => {
+    const {eachTab, onChangeTab} = this.props
+    onChangeTab(eachTab.tabId)
+  }
+
   render() {
-    const {eachTab} = this.props
+    const {eachTab, isActive} = this.props
+
+    const check = isActive ? 'selectedTab' : null
     return (
       <li className="tabItem">
-        <button type="button" className="tabBtn">
+        <button
+          type="button"
+          onClick={this.onClick}
+          className={`tabBtn ${check}`}
+        >
           {eachTab.displayText}
         </button>
       </li>
